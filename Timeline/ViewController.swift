91dae98d51d99f9999dd52ead8d660a85711f8d3
Steven <mitchs.patterson@gmail.com>
2016-08-08 13:43:28 -0600

    Initial Commit

---
+++ Timeline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timeline
+//
+//  Created by Steven Patterson on 8/8/16.
+//  Copyright © 2016 Steven Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
